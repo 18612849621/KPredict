8ca4b5358c36a5fd3e6a1835f2a6efb17c3ce5a3
18612849621 <1354368242@qq.com>
2024-11-30 03:16:02 +0000

    重新初始化

--- README.md
+++ README.md
@@ -2,7 +2,13 @@
 在线推理框架
 
 # abstract
-本框基于 cuda11.8 + tensorRt8.6 搭建
+本框基于 cuda11.8 搭建
 
 # compile
 
+```shell
+mkdir build
+cd build
+cmake ..
+make -j8
+```
--- src/main.cc
+++ src/main.cc
@@ -1,21 +1,6 @@
-#include <NvInfer.h>
-#include <cassert>
-#include <cuda_runtime_api.h>
 #include <iostream>
 
-class Logger : public nvinfer1::ILogger {
-public:
-  // 在这里添加 noexcept
-  void log(Severity severity, const char *msg) noexcept override {
-    if (severity != Severity::kINFO) // 只显示警告和错误信息
-      std::cout << msg << std::endl;
-  }
-};
-
 int main() {
-  // 创建 Logger
-  Logger logger;
-
-  std::cout << "TensorRT installation is working correctly!" << std::endl;
+  std::cout << "hello, world!" << std::endl;
   return 0;
 }
