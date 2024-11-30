0e57d8ebef6eb5cf930f4680db21908050eeb592
18612849621 <1354368242@qq.com>
2024-11-30 09:49:35 +0000

    添加设备type 增加申请和释放接口

--- src/memory/memory_allocator.h
+++ src/memory/memory_allocator.h
@@ -1,7 +1,15 @@
 #pragma once
+#include <cstddef>
 
 class MemoryAllocator {
+  enum class DeviceType : int { CPU = 0, GPU = 1, UNKOWN = 2 };
+
 public:
-  MemoryAllocator() = default;
-  virtual ~MemoryAllocator() = 0;
+  MemoryAllocator()                      = default;
+  virtual ~MemoryAllocator()             = 0;
+  virtual void *Allocate(size_t) const   = 0;
+  virtual void  Deallocate(void *) const = 0;
+
+private:
+  DeviceType device_type_ = DeviceType::UNKOWN;
 };
